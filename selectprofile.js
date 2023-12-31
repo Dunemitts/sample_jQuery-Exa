cac13ed03cf76071bf46105b8a504f5db6fc4760
Dunemitts <117410186+Dunemitts@users.noreply.github.com>
2023-12-30 21:59:40 -0500

    init commit

---
+++ selectprofile.js
@@ -0,0 +1,9 @@
+const setHeadings = () => {
+    $("#heading1").text("Kevin");
+    $("#heading2").text("Computer Science");
+    $("#heading3").text("2nd Year");
+    $("#heading4").html("<em>Europe</em>");
+}
+
+
+$(document).ready(setHeadings);
